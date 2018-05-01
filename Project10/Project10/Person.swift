9e6d9070cb4ee664f01920f9590823537f703f88
Administrator <Administrator@adminisorsimac2.ctmh.com>
2018-05-01 15:06:31 -0600

    End of project 10

---
+++ Project10/Project10/Person.swift
@@ -0,0 +1,23 @@
+//
+//  Person.swift
+//  Project10
+//
+//  Created by Administrator on 5/1/18.
+//  Copyright © 2018 SmartApps. All rights reserved.
+//
+
+import UIKit
+
+class Person: NSObject {
+	
+	var name: String
+	var image: String
+	
+	init(name: String, image: String) {
+		self.name = name
+		self.image = image
+	}
+	
+	
+
+}
---
+++ Project10/Project10/PersonCell.swift
@@ -0,0 +1,17 @@
+//
+//  PersonCell.swift
+//  Project10
+//
+//  Created by Administrator on 5/1/18.
+//  Copyright © 2018 SmartApps. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+	
+	@IBOutlet var imageView: UIImageView!
+	@IBOutlet var name: UILabel!
+	
+    
+}

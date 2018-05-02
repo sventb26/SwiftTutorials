f8dd5343c4e07b47e6ae2ab1adabba8554ac8721
Administrator <Administrator@adminisorsimac2.ctmh.com>
2018-05-02 15:41:12 -0600

    Merged branch 20180502-Project12-Coding into master

--- Project10/Project10/Person.swift
+++ Project10/Project10/Person.swift
@@ -9,15 +9,15 @@
 import UIKit
 
 class Person: NSObject, Codable {
-	
-	var name: String
-	var image: String
-	
-	init(name: String, image: String) {
-		self.name = name
-		self.image = image
-	}
-	
-	
+    
+    var name: String
+    var image: String
+    
+    init(name: String, image: String) {
+        self.name = name
+        self.image = image
+    }
+    
+    
 
 }

c2ca81ffc11acee095410aed89aeb6a26b3b66eb
Administrator <Administrator@adminisorsimac2.ctmh.com>
2018-05-15 15:49:06 -0600

    Abandoned project 16.

---
+++ Project16/Extension/Action.js
@@ -0,0 +1,16 @@
+var Action = function() {};
+
+Action.prototype = {
+	
+run: function(parameters) {
+	parameters.completionFunction({"URL": document.URL, "title": document.title });
+},
+	
+finalize: function(parameters) {
+	
+}
+	
+};
+
+var ExtensionPreprocessingJS = new Action
+                                       
---
+++ Project16/Project16/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Project16
+//
+//  Created by Administrator on 5/11/18.
+//  Copyright © 2018 SmartApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+

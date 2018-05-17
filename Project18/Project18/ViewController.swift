a44e9980799b7b995120328a1c6d8bc5502c41d6
Sven ten Bosch <Administrator@adminisorsimac2.ctmh.com>
2018-05-17 11:40:54 -0600

    Empty project, just to demonstrate debugging tools.

---
+++ Project18/Project18/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Project18
+//
+//  Created by Administrator on 5/17/18.
+//  Copyright © 2018 SmartApps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		for i in 1 ... 100 {
+			print("Got number \(i)")
+		}
+		
+		
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
